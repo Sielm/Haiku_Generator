9977a9df58b03b1aa2a68f7b7101400243a7c2da
Nico <ngrande@chadwickschool.org>
2016-10-26 09:00:32 -0700

    added all the individual haiku lines, starting random generation

---
+++ haiku.py
@@ -0,0 +1,9 @@
+import random
+
+# Lists
+firstline = ['Breaking smooth turquoise', 'thick blanket of snow', 'dainty daffodil', 'music in the wind', 'ship crew goes on strike', 'Sudden summer rain', 'diamond dewdrops', 'liquid little stones', 'Gullies scar brown earth', 'talk of winter now'];
+secondline = ["Rapid breaths of frigid cold air", "snuggling the flowerbeds", "your golden trumpet fanfares", "beneath my tree's sombrero", "sailing the Caribbean", "soaks the garden before dusk", "glisten on gossamer threads", "skipping and skittering free", "Hurricanes bring erosion", "old dog by a crackling fire"];
+thirdline = ["Untamed waves fight back", "with a winter wrap", "the dawning of spring", "grass breathes sweet relief", "wooden leg splashing", "Cat licks early stars", "morning has broken", "on shared umbrellas", "Of both soil and lives", "dancing red and gold"];
+# Random Numbers
+x = randrange(0,10)
+print(firstline[x])
