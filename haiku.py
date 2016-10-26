86613758cb7358ad6a6ac570d0b90ee8a54f20fa
Nico <ngrande@chadwickschool.org>
2016-10-26 09:14:36 -0700

    can create 3 haikus randomly

--- haiku.py
+++ haiku.py
@@ -1,9 +1,16 @@
-import random
+from random import *
 
 # Lists
 firstline = ['Breaking smooth turquoise', 'thick blanket of snow', 'dainty daffodil', 'music in the wind', 'ship crew goes on strike', 'Sudden summer rain', 'diamond dewdrops', 'liquid little stones', 'Gullies scar brown earth', 'talk of winter now'];
 secondline = ["Rapid breaths of frigid cold air", "snuggling the flowerbeds", "your golden trumpet fanfares", "beneath my tree's sombrero", "sailing the Caribbean", "soaks the garden before dusk", "glisten on gossamer threads", "skipping and skittering free", "Hurricanes bring erosion", "old dog by a crackling fire"];
 thirdline = ["Untamed waves fight back", "with a winter wrap", "the dawning of spring", "grass breathes sweet relief", "wooden leg splashing", "Cat licks early stars", "morning has broken", "on shared umbrellas", "Of both soil and lives", "dancing red and gold"];
 # Random Numbers
-x = randrange(0,10)
-print(firstline[x])
+
+for i in range(3):
+    line1 = randrange(0,10)
+    line2 = randrange(0,10)
+    line3 = randrange(0,10)
+    print(firstline[line1])
+    print(secondline[line2])
+    print(thirdline[line3])
+    print()
