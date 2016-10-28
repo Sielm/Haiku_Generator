b4c6300fb00b633b3df869bf4168055e466b9599
Nico <ngrande@chadwickschool.org>
2016-10-28 12:22:24 -0700

    made capitalization consistent

--- haiku.py
+++ haiku.py
@@ -1,9 +1,9 @@
 from random import *
 
 # Lists
-firstline = ['Breaking smooth turquoise', 'thick blanket of snow', 'dainty daffodil', 'music in the wind', 'ship crew goes on strike', 'Sudden summer rain', 'diamond dewdrops', 'liquid little stones', 'Gullies scar brown earth', 'talk of winter now'];
-secondline = ["Rapid breaths of frigid cold air", "snuggling the flowerbeds", "your golden trumpet fanfares", "beneath my tree's sombrero", "sailing the Caribbean", "soaks the garden before dusk", "glisten on gossamer threads", "skipping and skittering free", "Hurricanes bring erosion", "old dog by a crackling fire"];
-thirdline = ["Untamed waves fight back", "with a winter wrap", "the dawning of spring", "grass breathes sweet relief", "wooden leg splashing", "Cat licks early stars", "morning has broken", "on shared umbrellas", "Of both soil and lives", "dancing red and gold"];
+firstline = ['Breaking smooth turquoise', 'Thick blanket of snow', 'Dainty daffodil', 'Music in the wind', 'Ship crew goes on strike', 'Sudden summer rain', 'Diamond dewdrops', 'Liquid little stones', 'Gullies scar brown earth', 'Talk of winter now'];
+secondline = ["Rapid breaths of frigid cold air", "Snuggling the flowerbeds", "Your golden trumpet fanfares", "Beneath my tree's sombrero", "Sailing the Caribbean", "Soaks the garden before dusk", "Glisten on gossamer threads", "Skipping and skittering free", "Hurricanes bring erosion", "Old dog by a crackling fire"];
+thirdline = ["Untamed waves fight back", "With a winter wrap", "The dawning of spring", "Grass breathes sweet relief", "Wooden leg splashing", "Cat licks early stars", "Morning has broken", "On shared umbrellas", "Of both soil and lives", "Dancing red and gold"];
 titlefirstword = ["Sulky", "Smiling", "Swift", "Wicked", "Eager", "Wary", "Verdant", "Rare", "Enchanting", "Peaceful", "Graceful"]; 
 titlesecondword = ["Thought", "Quartz", "Yak", "Flock", "Machine", "Cactus", "Thunder", "Lightning"];
 
